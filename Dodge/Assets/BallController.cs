7f3c701a6888417f314b86cc5fd3ece9e31e9e5d
RHJ0728 <rhj0728@naver.com>
2022-09-16 17:31:41 +0900

    20220916_2

---
+++ Dodge/Assets/BallController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody playerRigidbody; //리지드바디 컴포넌트
+    public 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
